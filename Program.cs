5613ef7d88cd70f6761a4a4bd39ee76c51bfcf56
Mihai <earlkingmajesty@gmail.com>
2024-11-05 04:17:35 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using Game;
+
+Unit player = new Unit(100, 20, 12, "Player");
+Unit enemy = new Unit(75, 10, 7, "Enemy Mage");
+Random random = new Random();
+
+while (!player.isDead && !enemy.isDead)
+{
+    Console.WriteLine(player.UnitName + " HP = " + player.Hp + ". " + enemy.UnitName + " HP = " + enemy.Hp);
+    Console.WriteLine("Player turn1! What will you do?");
+    string choice = Console.ReadLine();
+
+    if (choice == "a")
+        player.Attack(enemy);
+    else
+        player.Heal();
+
+    if (player.isDead || enemy.isDead) break;  
+
+    Console.WriteLine(player.UnitName + " HP = " + player.Hp + ". " + enemy.UnitName + " HP = " + enemy.Hp);
+
+    Console.WriteLine("Enemy turn!");
+
+    int rand = random.Next(0, 2);
+
+    if (rand == 0)
+        enemy.Attack(player);
+    else
+        enemy.Heal();
+}
